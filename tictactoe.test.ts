1de6dd2c93c6e8606a5330bca648cce23d14a3d1
antoRados <antorados@gmail.com>
2021-10-08 13:25:05 -0400

    Completed primary assessment tasks

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -1,4 +1,4 @@
-import { Builder, Capabilities, By } from "selenium-webdriver"
+import { Builder, Capabilities, By, Button } from "selenium-webdriver"
 
 const chromedriver = require('chromedriver')
 
@@ -12,9 +12,48 @@ afterAll(async () => {
     await driver.quit()
 })
 
-test('I can start a game', async () => {
+describe('Human player moves', () => {
 
-    let button = await (await driver).findElement(By.id('start-game'));
-    await button.click();
+    test('I can start a game', async () => {
+
+        let button = await (await driver).findElement(By.id('start-game'));
+        await button.click();
+        
+    });
     
+    test('Check that clicking the upper left square adds an X to the square.', async() => {
+    
+        let cell = await (await driver).findElement(By.id('cell-0'));
+        await cell.click();
+    
+    });
+    
+    test('Check that clicking the upper right square adds an X to the square.', async() => {
+    
+        let cell = await (await driver).findElement(By.id('cell-2'));
+        await cell.click();
+    
+    });
+    
+    test('Check that clicking the lower right square adds an X to the square.', async() => {
+    
+        let cell = await (await driver).findElement(By.id('cell-8'));
+        await cell.click();
+    
+    });
+
 });
+
+describe('AI player moves', () => {
+
+    test('Check to see that the computer moves to a square after clicking on a square.', async(computerMove) => {
+        
+        let playerMove = await (await driver).findElement(By.id('cell-4'));
+        await playerMove.click();
+
+        let cell = await (await driver).findElement(By.id('cell-0'));
+        await computerMove()
+
+    });
+
+})
